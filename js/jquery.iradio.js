bca91ac6fe001b10634e6319a96e262551592b1b
vxsx <vxsx@me.com>
2011-03-09 13:47:54 +0300

    added draggable funcitonality

--- js/jquery.iradio.js
+++ js/jquery.iradio.js
@@ -3,13 +3,14 @@
 *  NO COPYRIGHTS, DO WHAT YOU WANT
 *  @requires jquery 1.4.4 or higher
 */
-//TODO: fix speed 
+//TODO: fix speed -- done
 //		do vertical
 //		drag'n'drop support
 (function($){
     $.fn.iradio = function(options) {
         var settings = $.extend({
-			duration : 400
+			duration  : 500,
+			draggable : true
         }, options);
 		
 		var inputs = $(this).hide(),
@@ -23,27 +24,61 @@
 			knob = $('.knob', w).append('<span>'+$('label[for="'+inputs.filter(':checked').attr('id')+'"]').text()+'</span>'),
 			knobText = $('span', knob),
 			labels = $('label', w),
-			maxWidth = Math.max.apply(labels, $(labels).map(function(i,e){ return $(e).width() }).get() );
-			
+			maxWidth = Math.max.apply(labels, $(labels).map(function(i,e){ return $(e).width() }).get() ),
+			inputsCount = inputs.length;
+
+		
 			
 			labels.width(maxWidth);
 			knob.width(maxWidth);
 
-			
+		var wWidth = w.width(),
+			step = wWidth / inputsCount;
 
+		//knob speed
+		for (var i = 0; i < labels.length; i++ ) {
+			$(labels[i]).data('step',i);
+		}
+		var currentLabel = $('label[for="'+$(':checked', w).attr('id')+'"]');
+		
 		inputs.change(function(){
-			var checkedLabel = $('label[for="'+$(this).attr('id')+'"]'),
+			var	checkedLabel = $('label[for="'+$(this).attr('id')+'"]'),
 				checkedText = checkedLabel.text();
+			
+			//knob speed	
+			var cDuration = settings.duration * ( Math.abs(currentLabel.data('step') - checkedLabel.data('step'))) / inputsCount;
+			currentLabel = checkedLabel;
+				
 			if (knob.is(':animated'))
 			{
 				knob.stop();
 				knobText.stop();
 			}
-			knob.animate({'left': checkedLabel.position().left}, settings.duration, 'linear' )
-			knobText.animate({'opacity':'0.2'},  settings.duration/2, 'linear',  function() {
-				$(this).text(checkedText).animate({'opacity':'1'}, settings.duration/2 , 'linear')
+			knob.animate({'left': checkedLabel.position().left}, cDuration, 'linear' )
+			knobText.animate({'opacity':'0.2'},  cDuration/2, 'linear',  function() {
+				$(this).text(checkedText).animate({'opacity':'1'}, cDuration/2 , 'linear')
 			})
 		});
+			
+		settings.draggable && knob.draggable({
+	        containment: 'parent',
+	        axis: 'x',
+	        drag: function(event, ui) {
+				var currentStep =  parseInt((ui.position.left + knob.width() / 2) / step),
+					currentLabel = $(labels).eq(currentStep).text();
+				
+				if (knobText.text() != currentLabel) 	
+					knobText.animate({'opacity':'0.2'},  25, 'linear',  function() {
+						$(this).text(currentLabel).animate({'opacity':'1'}, 25 , 'linear')
+					}); //don't know if this really needed
+			},
+			stop: function(event, ui) {
+				var currentStep =  parseInt((ui.position.left + knob.width() / 2) / step);
+				knob.animate({'left': currentStep*step}, 200, 'linear')
+			}
+	    })
+	
+			
 			
         return this;
     };
